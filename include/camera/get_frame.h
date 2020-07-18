6dc7fb556b9ff4670a66c99d97d236798027831c
861514706@qq.com <861514706@qq.com>
2020-07-18 21:21:14 +0800

    [update] 增加拉流类

--- include/camera/get_frame.h
+++ include/camera/get_frame.h
@@ -5,9 +5,29 @@
 #ifndef FATIGUEDETECTIONSYSTEM_GET_FRAME_H
 #define FATIGUEDETECTIONSYSTEM_GET_FRAME_H
 
+#include <queue>
+#include <iostream>
+#include <unistd.h>
+#include <mutex>
+#include "opencv2/opencv.hpp"
 
-class get_frame {
+class GetFrame {
+public:
 
+    GetFrame(std::queue<cv::Mat> *frame_contain, std::mutex *frame_contain_lock,
+            const int _skip_interval);
+    void beginReadCamera(int camera_id=0);
+    void putFrame2Contain(const cv::Mat &frame);
+    void shutdownCamera(cv::VideoCapture &cap);
+    void restartCamera(cv::VideoCapture &cap, int camera_id);
+
+private:
+    // 视频帧容器
+    std::queue<cv::Mat> *_frame_contain;
+    // 容器锁
+    std::mutex *_frame_contain_lock;
+    // 跳帧频率
+    int _skip_interval;
 };
 
 
--- src/camera/get_frame.cpp
+++ src/camera/get_frame.cpp
@@ -1,5 +1,78 @@
-//
-// Created by 李陆沁 on 2020/7/18.
-//
+#include <camera/get_frame.h>
 
-#include "get_frame.h"
+GetFrame::GetFrame(std::queue<cv::Mat> *frame_contain, std::mutex *frame_contain_lock,
+        const int _skip_interval):
+        _frame_contain_lock(frame_contain_lock),
+        _frame_contain(frame_contain),
+        _skip_interval(_skip_interval) {
+}
+
+/**
+ * 将frame放入contain
+ * @param frame
+ */
+void GetFrame::putFrame2Contain(const cv::Mat &frame) {
+    this->_frame_contain_lock->lock();
+    this->_frame_contain->push(frame);
+    // 队列最大长度需要为2 以下行确保获取队列中存储的都是最新图像
+    if (this->_frame_contain->size() > 1) {
+        this->_frame_contain->pop();
+        this->_frame_contain_lock->unlock();
+    }
+    else {
+        this->_frame_contain_lock->unlock();
+        sleep(1);
+    }
+}
+
+/**
+ * 关闭摄像头
+ * @param cap
+ */
+void GetFrame::shutdownCamera(cv::VideoCapture &cap) {
+    cap.release();
+}
+
+/**
+ * 重新拉流
+ * @param cap
+ * @param camera_id
+ */
+void GetFrame::restartCamera(cv::VideoCapture &cap, int camera_id) {
+    std::cout << "[INFO] 正在重新拉流" << std::endl;
+    this->shutdownCamera(cap);
+    cap.open(camera_id);
+}
+
+/**
+ * 开始读取图片
+ * @param camera_id
+ */
+void GetFrame::beginReadCamera(int camera_id) {
+    int frame_idx = 0;
+    int failed_times = 0;
+    cv::VideoCapture cap(camera_id);
+    if (!cap.isOpened()) {
+        throw "[error] 视频流无法正常打开！";
+        exit(-1);
+    }
+    while (true) {
+        cv::Mat frame;
+        cap >> frame;
+        // 获取图像失败
+        if (frame.empty()) {
+            frame_idx = 0;
+            failed_times++;
+            std::cout << "[INFO] 获取图像失败" << failed_times << "次" << std::endl;
+            this->restartCamera(cap, camera_id);
+            if (failed_times == 3) break;
+            continue;
+        }
+        // 跳帧
+        frame_idx++;
+        if (frame_idx % this->_skip_interval != 0) continue;
+        this->putFrame2Contain(frame);
+    }
+    this->shutdownCamera(cap);
+}
+
