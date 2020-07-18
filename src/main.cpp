68938d40287e16e201ee4c4a586480a1b026c800
861514706@qq.com <861514706@qq.com>
2020-07-18 21:21:33 +0800

    [updata] 经过拉流测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
-//
-// Created by 李陆沁 on 2020/7/18.
-//
+#include <thread>
+#include "camera/get_frame.h"
+
+void getFrame(std::queue<cv::Mat> *frame_containt, std::mutex *frame_containt_lock) {
+    GetFrame camera(frame_containt, frame_containt_lock, 1);
+    camera.beginReadCamera(0);
+}
+
+int main() {
+    // 测试摄像头
+    std::queue<cv::Mat> frame_containt;
+    std::mutex frame_containt_lock;
+    std::thread getFrameThread(getFrame, &frame_containt, &frame_containt_lock);
+
+    while (true) {
+        if (!frame_containt.empty()) {
+            frame_containt_lock.lock();
+            cv::Mat frame = frame_containt.front();
+            frame_containt_lock.unlock();
+            cv::imshow("test", frame);
+            cv::waitKey(1);
+        }
+    }
+}
 
